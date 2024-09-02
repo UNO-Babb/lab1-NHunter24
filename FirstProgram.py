65d7e1c8913fbe640a0c9d93af63329b7cb41f4b
NHunter24 <nicholashunter@unomaha.edu>
2024-09-02 02:22:27 +0000

    Finished Lab 1

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,20 +1,25 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Nick Hunter
+#Date: 9/1/24
+#Assignment: FirstProgram
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("Hello")
   #Ask for the user's name
+  name = input("Enter your name: ")
 
   #Use the user's name in the program.
-
+  print("Nice to meet you", name)
   #Ask the user for their age.
-
+  age = input("Enter your age: ")
+  age = int(age) #convert from words to number
   #Tell the user what year they were born in.
+  born = 2024 - age
+
   #Assume that they have not had their birthday yet this year.
+  print("You were born in", born)
 
 
 #Call the main function if this is the file being run.
--- MadLib.py
+++ MadLib.py
@@ -1,12 +1,18 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Nick Hunter
+#Date: 9/1/24
+#Assignment: MadLib
 
 def main():
-  print("Madlib")
+  print("MyBizzareStory")
   #Ask user for words
-
+  verb1 = input("Enter a verb: ")
+  noun1 = input("Enter an animal: ")
+  noun2 = input("Enter a noun: ")
+  name1 = input("Enter a name: ")
+  adj1 = input("Enter an adjective: ")
+  food1 = input("Enter a food: ")
+  print("I was" , verb1 , "with my" , noun1 , "when I heard a" , adj1 , "noise." , "I went to the other room and saw" , name1 , "fighting my" , noun1 , "and" , verb1 , "my" , food1 + ".")
   #Print the story with the user supplied words.
 
 
